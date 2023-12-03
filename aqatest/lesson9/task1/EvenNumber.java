de47601e9dc67087591cddd1adcbd3455a2de60c
mac <leonidkovaliov1403@gmail.com>
2023-12-03 23:34:53 +0300

    add class EvenNumber

---
+++ aqatest/lesson9/task1/EvenNumber.java
@@ -0,0 +1,20 @@
+package aqatest.lesson9.task1;
+
+import java.util.Random;
+
+public class EvenNumber {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int[] numbers = new int[10];
+
+        int count = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = random.nextInt(100);
+            if (numbers[i] % 2 == 0) {
+                count++;
+            }
+        }
+
+        System.out.println("Количество четных чисел: " + count);
+    }
+}
